5d4dce1a6d5eb1ce52bb2517e55523505f542bb2
Matthew Richards <matthewrichards234@gmail.com>
2026-07-04 16:51:56 -0400

    Add headers to contact info.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,14 @@ const Contact = () => {
                     text-white
                     min-h-screen"
     >
-      Contact
+      <p>Contact</p>
+      <h1 className="text-shadow-2xs text-9xl flex flex-row items-center justify-center">
+        Contact Me
+      </h1>
+      <h3>
+        For any business inquires, please contact me with the information
+        provided below
+      </h3>
     </div>
   );
 };
