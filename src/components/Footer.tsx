e0e87aa146d8fe82ee7318a5160897bcda9c6200
Matthew Richards <matthewrichards234@gmail.com>
2026-07-04 16:17:15 -0400

    add links to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p>Footer</p>
+      <div className="flex flex-row gap-10">
+        <a
+          href="https://www.linkedin.com/in/matthew-richards234/"
+          target="_blank"
+        >
+          LINKEDIN
+        </a>
+        <a href="https://github.com/matthewrichards234" target="_blank">
+          GITHUB
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
