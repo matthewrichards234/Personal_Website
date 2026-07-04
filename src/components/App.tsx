0b1e392a787168e05582fb5e854e7515cea98c60
Matthew Richards <matthewrichards234@gmail.com>
2026-07-04 14:29:09 -0400

    implement basic routing & create Navbar component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import Landing from "./Landing";
 import About from "./About";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Landing />
-      <About />
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/" element={<Landing />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 };
