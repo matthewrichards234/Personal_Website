a9502f4e4049f13ad9cd74ce34fcc448ad5e8b67
Matthew Richards <matthewrichards234@gmail.com>
2026-07-05 19:05:17 -0400

    add placeholder email icon & working link to send emails to matthew's email.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,14 +5,22 @@ const Contact = () => {
                     text-white
                     min-h-screen"
     >
-      <p>Contact</p>
-      <h1 className="text-shadow-2xs text-9xl flex flex-row items-center justify-center">
-        Contact Me
-      </h1>
-      <h3>
-        For any business inquires, please contact me with the information
-        provided below
-      </h3>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-shadow-2xs text-9xl">Contact Me</h1>
+        <h3 className="text-3xl">
+          For any business inquires, please contact me with the information
+          provided below
+        </h3>
+        <div className="flex flex-row items-center gap-2">
+          <img src="src/assets/mail.png" alt="email icon" />
+          <a
+            href="mailto:matthewrichards234@gmail.com"
+            className="hover:text-red-700"
+          >
+            Matthewrichards234@gmail.com
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
