1fbff94f08212617e26332ee67d043a1cb91fc95
Matthew Richards <matthewrichards234@gmail.com>
2026-07-06 08:27:43 -0400

    change value of cover for scroll appear animation. delete too many boxes.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,18 +14,8 @@ const Projects = () => {
         items-center 
         animate-scrollAppear 
         [animation-timeline:view()] 
-        [animation-range:entry_0%_cover_40%]"
+        [animation-range:entry_0%_cover_20%]"
       >
-        <div className="w-80 h-12 bg-gray-300 rounded-lg"></div>
-        <div className="w-40 h-40 bg-gray-300 rounded-lg"></div>
-        <div className="w-64 h-24 bg-gray-300 rounded-lg"></div>
-        <div className="w-28 h-56 bg-gray-300 rounded-lg"></div>
-        <div className="w-96 h-16 bg-gray-300 rounded-lg"></div>
-        <div className="w-80 h-12 bg-gray-300 rounded-lg"></div>
-        <div className="w-40 h-40 bg-gray-300 rounded-lg"></div>
-        <div className="w-64 h-24 bg-gray-300 rounded-lg"></div>
-        <div className="w-28 h-56 bg-gray-300 rounded-lg"></div>
-        <div className="w-96 h-16 bg-gray-300 rounded-lg"></div>
         <div className="w-80 h-12 bg-gray-300 rounded-lg"></div>
         <div className="w-40 h-40 bg-gray-300 rounded-lg"></div>
         <div className="w-64 h-24 bg-gray-300 rounded-lg"></div>
