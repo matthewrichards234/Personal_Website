440f7021511ac8b9cb5aded2e0b9bdfb8844e1d4
Matthew Richards <matthewrichards234@gmail.com>
2026-07-09 14:01:48 -0400

    add individual scroll to each card.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,19 +8,33 @@ const ProjectCard = ({ images, technologies, description }: ProjectCardI) => {
   return (
     <div>
       {/* Card */}
-      <div className="border shadow-2xl w-96 h-120 rounded-xl">
+      <div
+        className="border 
+                      shadow-2xl 
+                      w-250 
+                      h-120 
+                      rounded-xl 
+                      overflow-hidden"
+      >
         {/* Card Content (images and text) */}
         <div className="flex flex-row h-full">
           {/* Card images (with scroll functionality) */}
-          <div className="bg-amber-500">
-            <div className="flex flex-col">
+          <div className="bg-amber-500 w-1/2 h-full">
+            <div className="flex flex-col overflow-y-auto h-full">
               {images.map((image) => (
-                <div key={image}>{image}</div>
+                <div key={image} className="border h-100 shrink-0">
+                  {image}
+                </div>
               ))}
             </div>
           </div>
           {/* Card text (title, technologies used, description, etc) */}
-          <div className="flex flex-col bg-blue-500">
+          <div
+            className="flex
+                      flex-col 
+                    bg-blue-500 
+                      w-1/2"
+          >
             {/* Technologies */}
             <div className="flex flex-col">
               {technologies.map((technology) => (
