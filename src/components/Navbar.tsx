8b9f622901ed49a2ed9432c16a318b2e46f88782
Matthew Richards <matthewrichards234@gmail.com>
2026-07-10 12:52:17 -0400

    style navbar and delete unused images.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,40 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="flex flex-row">
-        <div>
+    <div
+      className="font-manrope
+                  font-normal"
+    >
+      <nav
+        className="flex 
+                  flex-row
+                  pt-3
+                  h-12
+                  "
+      >
+        <div className="pl-2">
           <NavLink to="/">LOGO</NavLink>
         </div>
-        <div className="flex flex-row gap-2 pl-15">
+
+        <div
+          className="flex 
+                        flex-row 
+                        gap-2 
+                        ml-auto 
+                        mr-4"
+        >
           <NavLink to="/about" className="hover:underline">
             ABOUT
           </NavLink>
+
           <NavLink to="/projects" className="hover:underline">
             PROJECTS
           </NavLink>
+
           <NavLink to="/resume" className="hover:underline">
             RESUME
           </NavLink>
+
           <NavLink to="/contact" className="hover:underline">
             CONTACT
           </NavLink>
