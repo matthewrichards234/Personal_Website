b4c892d42ba074eb43cdf461b063decf8ea39a37
Matthew Richards <matthewrichards234@gmail.com>
2026-07-18 14:46:09 -0400

    fix layout of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,25 +5,27 @@ const Footer = () => {
                   font-manrope
                   font-normal"
     >
-      <div className="flex flex-row gap-10">
-        <p>&copy; Matthew Richards 2026</p>
-        <a
-          href="https://www.linkedin.com/in/matthew-richards234/"
-          target="_blank"
-        >
-          <img
-            src="src/assets/linkedin-svgrepo-com.svg"
-            alt="linkedin-icon"
-            className="h-16 w-16"
-          />
-        </a>
-        <a href="https://github.com/matthewrichards234" target="_blank">
-          <img
-            src="src/assets/github-svgrepo-com.svg"
-            alt="github icon"
-            className="h-16 w-16"
-          />
-        </a>
+      <div className="flex flex-row mt-1.5">
+        <p className="mt-auto mb-auto ml-3.5">&copy; Matthew Richards 2026</p>
+        <div className="flex flex-row gap-2 ml-auto mr-3">
+          <a
+            href="https://www.linkedin.com/in/matthew-richards234/"
+            target="_blank"
+          >
+            <img
+              src="src/assets/linkedin-svgrepo-com.svg"
+              alt="linkedin-icon"
+              className="h-16 w-16"
+            />
+          </a>
+          <a href="https://github.com/matthewrichards234" target="_blank">
+            <img
+              src="src/assets/github-svgrepo-com.svg"
+              alt="github icon"
+              className="h-16 w-16"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
