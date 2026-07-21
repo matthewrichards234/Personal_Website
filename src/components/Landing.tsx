a9ac4e5c0a44bd55f0473293fac1936526be69ba
Matthew Richards <matthewrichards234@gmail.com>
2026-07-21 18:40:45 -0400

    style landing page.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,14 +1,36 @@
 const Landing = () => {
+  const pulses = Array.from({ length: 10 }, (value, index) => index);
   return (
     <div
       className="bg-blue-900
                     text-white
                     w-screen 
-                    h-screen "
+                    h-screen 
+                    animate-colorAnimate"
     >
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black z-2">
         <h1 className="text-[400px] font-bold">MR</h1>
       </div>
+      {pulses.map((i) => (
+        <span
+          key={i}
+          style={{ "--i": i } as React.CSSProperties}
+          className="
+                    absolute
+                    top-1/2
+                    left-1/2
+                    -translate-x-1/2
+                    -translate-y-1/2
+                    h-170
+                    w-170
+                    rounded-full
+                    bg-white
+                    opacity-70
+                    animate-pulseAnimate
+                    [animation-delay:calc(1s*var(--i))]
+                  "
+        />
+      ))}
     </div>
   );
 };
