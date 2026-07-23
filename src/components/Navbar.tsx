1d8b009fdd19099f66bd9e64d9816fcea1d297b7
Matthew Richards <matthewrichards234@gmail.com>
2026-07-23 14:56:48 -0400

    split navbar into mobile and desktop views.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,79 +14,107 @@ const Navbar = () => {
                   bg-white
                   z-100"
     >
-      <nav
-        className="flex 
+      {/* Mobile view */}
+      <div className="block md:hidden">
+        {" "}
+        <nav
+          className="flex 
                   flex-row
                   "
-      >
-        <div className="pl-2">
-          <NavLink to="/">
-            <p
-              className="font-bold 
+        >
+          <div className="pl-2">
+            <NavLink to="/">
+              <p
+                className="font-bold 
               text-[24px]
               transform
               transition-transform
               duration-400
               ease-in-out
               hover:scale-130"
-            >
-              MR
-            </p>
-          </NavLink>
-        </div>
-
-        <div
+              >
+                MR
+              </p>
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+      {/* Desktop view */}
+      <div className="hidden md:block">
+        <nav
           className="flex 
+                  flex-row
+                  "
+        >
+          <div className="pl-2">
+            <NavLink to="/">
+              <p
+                className="font-bold 
+              text-[24px]
+              transform
+              transition-transform
+              duration-400
+              ease-in-out
+              hover:scale-130"
+              >
+                MR
+              </p>
+            </NavLink>
+          </div>
+
+          <div
+            className="flex 
                         flex-row 
                         gap-5 
                         ml-auto 
                         mr-4"
-        >
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              isActive
-                ? "text-red-500 hover:underline"
-                : "text-black hover:underline"
-            }
           >
-            ABOUT
-          </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-red-500 hover:underline"
+                  : "text-black hover:underline"
+              }
+            >
+              ABOUT
+            </NavLink>
 
-          <NavLink
-            to="/projects"
-            className={({ isActive }) =>
-              isActive
-                ? "text-red-500 hover:underline"
-                : "text-black hover:underline"
-            }
-          >
-            PROJECTS
-          </NavLink>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-red-500 hover:underline"
+                  : "text-black hover:underline"
+              }
+            >
+              PROJECTS
+            </NavLink>
 
-          <NavLink
-            to="/resume"
-            className={({ isActive }) =>
-              isActive
-                ? "text-red-500 hover:underline"
-                : "text-black hover:underline"
-            }
-          >
-            RESUME
-          </NavLink>
+            <NavLink
+              to="/resume"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-red-500 hover:underline"
+                  : "text-black hover:underline"
+              }
+            >
+              RESUME
+            </NavLink>
 
-          <NavLink
-            to="/contact"
-            className={({ isActive }) =>
-              isActive
-                ? "text-red-500 hover:underline"
-                : "text-black hover:underline"
-            }
-          >
-            CONTACT
-          </NavLink>
-        </div>
-      </nav>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-red-500 hover:underline"
+                  : "text-black hover:underline"
+              }
+            >
+              CONTACT
+            </NavLink>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
