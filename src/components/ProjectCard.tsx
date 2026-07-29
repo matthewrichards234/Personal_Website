4f7ccb9ae82c6a062503ff9d1c5eb978e2166355
Matthew Richards <matthewrichards234@gmail.com>
2026-07-29 07:43:36 -0400

    Link storybook to tailwind. Create classnames for each element in ProjectCard for storybook styling.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,26 @@
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+interface ProjectCardI {
+  title: string;
+  coverImage: string;
+  titleClassName: string;
+  imageClassName: string;
+}
+
+const ProjectCard = ({
+  title,
+  coverImage,
+  titleClassName,
+  imageClassName,
+}: ProjectCardI) => {
+  return (
+    <div>
+      <p className={titleClassName}>{title}</p>
+      <img
+        src={coverImage}
+        className={imageClassName}
+        alt="Project Card Cover Image"
+      />
+    </div>
+  );
 };
 
 export default ProjectCard;
--- src/stories/ProjectCard.stories.jsx
+++ src/stories/ProjectCard.stories.jsx
@@ -6,23 +6,11 @@ export default {
   component: ProjectCard,
 };
 
-const Template = (args) => <ProjectCard {...args} />;
+export const Primary = (args) => <ProjectCard {...args} />;
 
-export const ProjectCardSB = Template.bind({});
-
-ProjectCardSB.args = {
+Primary.args = {
   title: "WTWR",
-  images: [imgTest, imgTest, imgTest],
-  technologies: [
-    "React",
-    "JavaScript",
-    "Mongoose",
-    "Express",
-    "Google VM Cloud Hosting",
-    "Joi",
-    "HTML / CSS",
-  ],
-  description:
-    "WTWR is a full-stack weather application that provides personalized outfit recommendations based on real-time weather conditions in a user's city. Built with React, Redux, and React Router, the application delivers a responsive user experience through reusable components, custom hooks, and centralized state management. A Node.js and Express.js backend handles user authentication and securely manages user data, creating a seamless experience that combines dynamic weather information with personalized recommendations.",
-  githubLink: "https://github.com/matthewrichards234/se_project_react",
+  coverImage: imgTest,
+  titleClassName: "text-2xl font-bold",
+  imageClassName: "size-50",
 };
