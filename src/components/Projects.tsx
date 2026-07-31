280a089bb24a65d21eabe83e37a78c576b711d67
Matthew Richards <matthewrichards234@gmail.com>
2026-07-31 18:30:20 -0400

    add responsive design on project cards to projects page.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,15 +17,15 @@ const Projects = () => {
         A brief tour of some of my favorite projects
       </h2>
       <div>
-        <nav className="flex flex-col gap-5 p-3 items-center">
+        <nav className="flex flex-col lg:flex-row lg:flex-wrap lg:gap-7 gap-5 p-3 items-center justify-center">
           {projectList.map((project) => {
             return (
               <Link to={`${project.replaceAll(" ", "-")}`}>
                 <ProjectCard
                   title={project}
-                  projectCardClassName="border bg-white rounded-2xl flex flex-col justify-center items-center shadow-2xl p-2 size-67"
-                  titleClassName="text-2xl font-bold text-center text-black"
-                  imageClassName="size-55 rounded-2xl shadow-2xl"
+                  projectCardClassName="border hover:scale-105 ease-in-out duration-300 bg-white rounded-2xl flex flex-col justify-center items-center shadow-2xl p-2 size-67 sm:size-80 md:size-120 lg:w-150 lg:h-90"
+                  titleClassName="text-2xl md:text-3xl pb-2 font-bold text-center text-black"
+                  imageClassName="size-55 sm:size-60 md:size-70 lg:size-75 rounded-2xl shadow-2xl"
                   coverImage={imgTest}
                 />
               </Link>
