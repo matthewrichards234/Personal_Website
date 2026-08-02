d8a3a504a210bf4116348ac4da5677d6aa3a5255
Matthew Richards <matthewrichards234@gmail.com>
2026-08-02 11:41:38 -0400

    Conditionally render either graphic resume or default resume on resume web page.

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 import resumePDF from "../assets/resume_for_website.pdf";
 import downloadIcon from "../assets/download-minimalistic-svgrepo-com.svg";
 import resumeScreenshot from "../assets/resume_screenshot.png";
+import graphicResumeScreenshot1 from "../assets/graphic-resume-1.png";
+import graphicResumeScreenshot2 from "../assets/graphic-resume-2.png";
 
 const Resume = () => {
+  const [activeResume, setActiveResume] = useState("default");
+
   function handleDownloadClickEvent(fileUrl: string, fileName: string): void {
-    console.log("fileUrl", fileUrl);
-    console.log("fileName", fileName);
     const link = document.createElement("a");
     link.href = fileUrl;
     link.setAttribute("download", fileName);
@@ -13,10 +17,19 @@ const Resume = () => {
     link.click();
     document.body.removeChild(link);
   }
+
+  function setDefaultResume(): void {
+    setActiveResume("default");
+  }
+
+  function setGraphicResume(): void {
+    setActiveResume("graphic");
+  }
+
   return (
     <div className="bg-blue-900 w-screen h-screen text-white font-manrope">
       <div className="flex flex-row justify-center items-center lg:gap-1.5">
-        <h1 className="text-4xl lg:text-8xl p-3 lg:p-4.5">My Resume</h1>
+        <h1 className="text-4xl lg:text-8xl p-3 mt-1 lg:p-4.5">My Resume</h1>
         <button
           className="cursor-pointer"
           onClick={() => {
@@ -30,12 +43,40 @@ const Resume = () => {
           />
         </button>
       </div>
-      <div className="flex justify-center pt-10 md:pt-3 lg:pt-1">
-        <img
-          src={resumeScreenshot}
-          alt="Matthew Richards Resume"
-          className="max-h-[80vh] w-auto shadow-2xl"
-        />
+      <div className="flex flex-row justify-evenly bg-white text-black w-[80%] rounded-2xl mx-auto mt-2">
+        <button
+          onClick={() => {
+            setDefaultResume();
+          }}
+        >
+          standard resume
+        </button>
+        <button
+          onClick={() => {
+            setGraphicResume();
+          }}
+        >
+          graphic resume
+        </button>
+      </div>
+      <div className="flex justify-center pt-5 md:pt-3 lg:pt-1">
+        {activeResume == "default" ? (
+          <>
+            <img
+              src={resumeScreenshot}
+              alt="Matthew Richards Resume"
+              className="max-h-[80vh] w-auto shadow-2xl p-1.5"
+            />
+          </>
+        ) : (
+          <>
+            <img
+              src={graphicResumeScreenshot1}
+              alt="Matthew Richards Graphic Resume"
+              className="max-h-[80vh] w-auto shadow-2xl p-1.5"
+            />
+          </>
+        )}
       </div>
     </div>
   );
